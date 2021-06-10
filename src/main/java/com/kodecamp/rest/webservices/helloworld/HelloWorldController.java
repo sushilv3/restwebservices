1463c9bda572b1f8bf5e35b47609ef94117582ce
sushilv3 <sushilv3rma@gmail.com>
2021-06-10 20:01:31 +0530

    set requestHeader accept-Language into request mappingg

--- src/main/java/com/kodecamp/rest/webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/kodecamp/rest/webservices/helloworld/HelloWorldController.java
@@ -1,13 +1,22 @@
 package com.kodecamp.rest.webservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.LocaleResolver;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 
 //	@GetMapping("/welcome")
 	@RequestMapping(method = RequestMethod.GET,path="/welcome")
@@ -23,5 +32,10 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET,path="/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("hello world bean, %s",name));
+	}
+	
+	@RequestMapping(method = RequestMethod.GET,path="/hello-world-internationalized")
+	public String helloWorld(@RequestHeader(name="Accept-Language",required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message",null, locale);
 	}
 }
