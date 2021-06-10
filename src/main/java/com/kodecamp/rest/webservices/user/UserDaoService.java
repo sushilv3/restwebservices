e35e127ac1fbb6b3e643de7b376890e2fdb59b40
sushilv3 <sushilv3rma@gmail.com>
2021-06-10 11:22:42 +0530

    applied findByid and deleteById methods

--- src/main/java/com/kodecamp/rest/webservices/user/UserDaoService.java
+++ src/main/java/com/kodecamp/rest/webservices/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.kodecamp.rest.webservices.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -35,6 +36,18 @@ public class UserDaoService {
 	public User findById(int id) {
 		for (User user : users) {
 			if (user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	public User deleteById(int id) {
+		Iterator<User> iterator = users.iterator();
+		while(iterator.hasNext()) {
+			User user = iterator.next();
+			if(user.getId()==id) {
+				iterator.remove();
 				return user;
 			}
 		}
