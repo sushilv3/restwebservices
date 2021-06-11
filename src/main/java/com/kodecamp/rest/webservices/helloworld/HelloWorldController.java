ede975e1cc371e161f502aeeebf260715c524ca4
sushilv3 <sushilv3rma@gmail.com>
2021-06-11 08:42:33 +0530

    remove request header

--- src/main/java/com/kodecamp/rest/webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/kodecamp/rest/webservices/helloworld/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -35,7 +36,7 @@ public class HelloWorldController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET,path="/hello-world-internationalized")
-	public String helloWorld(@RequestHeader(name="Accept-Language",required=false) Locale locale) {
-		return messageSource.getMessage("good.morning.message",null, locale);
+	public String helloWorld() {
+		return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
 	}
 }
